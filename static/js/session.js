d8d997b51d90b9ee72377af59bb3f4b3de4df4e2
Glenn Y. Rolland <glenux@glenux.net>
2011-05-14 00:30:13 +0200

    js session: added an AJAX call to get user_id.

--- static/js/session.js
+++ static/js/session.js
@@ -2,12 +2,28 @@
 function Session( callback ) {
 	this.brush = null;
 
+	this.SESSION_JOIN = "/api/session/join";
+	this.SESSION_LEAVE = "/api/session/leave";
+
+
 	var session_id = null;
 	var user_id = null;
 
 	function initialize() {
-		alert('getting user id');
 		// get session info from 
+		$.ajax({
+		    // FIXME: request with previous user_id
+		    url: "/api/session/join",
+		    ataType: "json",
+		    type: 'GET',
+		    context: this,
+		    success: function( user_id ){
+			this.user_id = user_id;
+
+			// FIXME: set cookie with user_id
+			alert('got user id : ' + this.user_id );
+		    }
+		});
 	}
 
 	initialize();
