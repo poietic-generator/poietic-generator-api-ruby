648d1fa3643390fc92cf55558b4d65897e59b214
Glenn Y. Rolland <glenux@glenux.net>
2011-05-14 17:13:05 +0200

    js session: declared some SESSION_* constants.

--- static/js/session.js
+++ static/js/session.js
@@ -1,38 +1,31 @@
 
+const SESSION_URL_JOIN = "/api/session/join";
+const SESSION_URL_LEAVE = "/api/session/leave";
+const SESSION_TYPE_DRAW = "draw";
+const SESSION_TYPE_VIEW = "view";
+
 function Session( session_type, callback ) {
 	this.brush = null;
-
-
-	this.TYPE
-
 
 	var session_id = null;
 	this.user_id = null;
 
-	function initialize() {
-		// get session info from 
-		$.ajax({
-		    // FIXME: request with previous user_id
-		    url: "/api/session/join?type="+session_type,
-		    ataType: "json",
-		    type: 'GET',
-		    context: this,
-		    success: function( user_id ){
-			this.user_id = user_id;
-			
-			// FIXME: set cookie with user_id for next time
-			alert('got user id : ' + this.user_id );
+	// get session info from 
+	$.ajax({
+	    // FIXME: request with previous user_id
+	    url: SESSION_URL_JOIN + "?type=" + session_type,
+	    ataType: "json",
+	    type: 'GET',
+	    context: this,
+	    success: function( user_id ){
+		this.user_id = user_id;
 
-			callback( this );
-		    }
-		});
-	}
+		// FIXME: set cookie with user_id for next time
+		console.log('got user id : ' + this.user_id );
 
-	initialize();
+		callback( this );
+	    }
+	});
 }
 
-Session.prototype.SESSION_JOIN = "/api/session/join";
-Session.prototype.SESSION_LEAVE = "/api/session/leave";
-Session.prototype.TYPE_DRAW = "draw";
-Session.prototype.TYPE_VIEW = "view";
 
