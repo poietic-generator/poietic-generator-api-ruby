e88dd75034b367a0fb6286ab26df461efac08906
Glenn Y. Rolland <glenux@glenux.net>
2011-05-18 16:56:56 +0200

    js drawing: Display neighbours zones in drawing (fixes issue #161).

--- static/js/drawing.js
+++ static/js/drawing.js
@@ -1,10 +1,13 @@
 
 const DRAWING_REFRESH = 5000;
+const DRAWING_COLOR_GRID = '#444';
+const DRAWING_COLOR_GRID_BOUDARIES = '#844';
 const DRAWING_URL_LIST = "/api/drawing/list";
 
 function Drawing( session_obj, canvas_id ){
+    var self = this;
 
-    this.load_remote = function(){
+    this.pull_patches = function(){
 		$.ajax({
 		    // FIXME: request with previous user_id
 		    url: DRAWING_URL_LIST,
@@ -24,23 +27,32 @@ function Drawing( session_obj, canvas_id ){
      * compute canvas_obj size (square) depending on screen size
      */
     this.draw_grid = function() {
-	var ctx = this.context;
-	var canvas = this.canvas_obj;
+	var ctx = self.context;
+	var canvas = self.canvas_obj;
 
 	ctx.fillStyle = '#000';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	var wshift = canvas.width / this.zone_width;
-	var hshift = canvas.height / this.zone_height;
-	console.log("wshift : "+wshift);
-	for (var w=0; w <= this.zone_width; w++){
-	    ctx.moveTo(w * wshift, 0);
-	    ctx.lineTo(w * wshift, canvas.height);
+	for (var w=0; w <= (2 * this.zone_width); w++){
+	    ctx.moveTo(w * self.zone_px_width, 0);
+	    ctx.lineTo(w * self.zone_px_width, canvas.height);
 	}
-	for (var h=0; h <= this.zone_height; h++){
-	    ctx.moveTo(0, h * hshift);
-	    ctx.lineTo(canvas.width, h * hshift);
+	for (var h=0; h <= (2 * this.zone_height); h++){
+	    ctx.moveTo(0, h * self.zone_px_height);
+	    ctx.lineTo(canvas.width, h * self.zone_px_height);
 	}
-	ctx.strokeStyle = '#444';
+	ctx.strokeStyle = DRAWING_COLOR_GRID;
+	ctx.stroke();
+
+	ctx.beginPath();
+	boundaries = {
+	    x : self.zone_width * self.zone_px_width / 2,
+	    y : self.zone_height * self.zone_px_height / 2,
+	    w : self.zone_width * self.zone_px_width,
+	    h : self.zone_height * self.zone_px_height
+	};
+	ctx.lineWidth = 5;
+	ctx.strokeRect( boundaries.x, boundaries.y, boundaries.w, boundaries.h );
+	ctx.strokeStyle = DRAWING_COLOR_GRID_BOUDARIES;
 	ctx.stroke();
     };
 
@@ -64,8 +76,8 @@ function Drawing( session_obj, canvas_id ){
 
 	console.log("window.width = " + [ $(window).width(), $(window).height() ] );
 
-	self.col_width = canvas.width / self.zone_width;
-	self.line_height = canvas.height / self.zone_height;
+	self.zone_px_width = canvas.width / (self.zone_width * 2);
+	self.zone_px_height = canvas.height / (self.zone_height * 2);
 
 	this.draw_grid();
     };
@@ -88,25 +100,38 @@ function Drawing( session_obj, canvas_id ){
 	var canvas = self.canvas_obj;
 	if (self.draw_enable) {
 		var zonecoord = {
-			x : Math.floor(event_obj.mouseX / self.col_width ) ,
-			y : Math.floor(event_obj.mouseY / self.line_height )
+			x : Math.floor(event_obj.mouseX / self.zone_px_width ) ,
+			y : Math.floor(event_obj.mouseY / self.zone_px_height )
 		};
 
 		console.log( "zonecoord : %s %s", zonecoord.x, zonecoord.y );
-		self.draw( zonecoord );
+		// FIXME: write coordinate conversion functions
+		// FIXME: write coodinates & boundaries verification functions
+		if ( ( zonecoord.x >= ( self.zone_width / 2) ) 
+			&& ( zonecoord.x < (( self.zone_width / 2 ) + self.zone_width ) )
+			&& ( zonecoord.y >= ( self.zone_height / 2) ) 
+			&& ( zonecoord.y < (( self.zone_height / 2 ) + self.zone_height ) ) )
+		  {
+		    self.set_pixel( zonecoord );
+		} else {
+		    self.get_pixel( zonecoord );
+		}
 
-		ctx.strokeStyle = '#0f0';
-		ctx.strokeRect( event_obj.mouseX - 1, event_obj.mouseY - 1, 3, 3 );
+		//ctx.strokeStyle = '#0f0';
+		//ctx.strokeRect( event_obj.mouseX - 1, event_obj.mouseY - 1, 1, 1 );
 	}
     };
 
-    this.draw = function( pos ) {
+    this.get_pixel = function( pos ) {
+    }
+
+    this.set_pixel = function( pos ) {
 	var ctx = self.context;
-	var xmin = pos.x * self.col_width;
-	var ymin = pos.y * self.line_height;
+	var xmin = pos.x * self.zone_px_width;
+	var ymin = pos.y * self.zone_px_height;
 
 	ctx.fillStyle = '#f00';
-	ctx.fillRect( xmin, ymin, self.col_width, self.line_height );
+	ctx.fillRect( xmin, ymin, self.zone_px_width, self.zone_px_height );
     };
 
     var canvas_event = function( event_obj ) {
@@ -128,13 +153,13 @@ function Drawing( session_obj, canvas_id ){
     this.canvas_id = canvas_id;
     this.canvas_obj = document.getElementById(canvas_id);
 
-    this.col_width = 1;
-    this.line_height = 1;
+    // size of zone's big pixels
+    this.zone_px_width = 1;
+    this.zone_px_height = 1;
 
-    this.timer = window.setInterval( this.load_remote, DRAWING_REFRESH );
+    this.timer = window.setInterval( this.pull_patches, DRAWING_REFRESH );
     this.context = this.canvas_obj.getContext('2d');
 
-    var self = this;
 
     this.canvas_obj.addEventListener('mousedown', canvas_event, false);
     this.canvas_obj.addEventListener('mouseup', canvas_event, false);
