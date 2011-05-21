60dc7ad9355284b7b183d553cf06189fa82267bb
Glenn Y. Rolland <glenux@glenux.net>
2011-05-21 21:30:36 +0200

    js colorpicker: * Added reference to session * Added reference to container object.

--- static/js/colorpicker.js
+++ static/js/colorpicker.js
@@ -2,13 +2,19 @@
 // vim: set ts=4 sw=4 et:
 "use strict";
 
-function ColorPicker() {
+function ColorPicker( p_session, p_picker_id ) {
 	var self = this;
 
 	this.update_size = function() {
-
+        // container 
+        
 	}
-	this.session = session;
 
+    this.to_s = function() { JSON.stringify(this); };
+
+	this.session = p_session;
+
+    var container_id = picker_id
+    var container = document.getElementById( p_picker_id );
 }
 
