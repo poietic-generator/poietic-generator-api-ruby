851a909a3d514aee4fea4b99c6a773864b7758b3
Glenn Y. Rolland <glenux@glenux.net>
2011-05-22 02:45:14 +0200

    js console: Added vim modeline.

--- static/js/console.js
+++ static/js/console.js
@@ -1,4 +1,7 @@
 
+// vim: set ts=4 sw=4 et:
+"use strict";
+
 if (!("console" in window) || !("firebug" in console)) {
     var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml", "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
     window.console = {};
