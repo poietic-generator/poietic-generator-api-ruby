10d81254a756acb4d4c8a4c1288123e80e59c24d
Glenn Y. Rolland <glenux@glenux.net>
2011-05-22 02:46:52 +0200

    js session: Use cookies for user_id & username.

--- static/js/session.js
+++ static/js/session.js
@@ -13,36 +13,57 @@ function Session( session_type, callback ) {
 
     var self = this;
 
-	this.brush = null;
-	this.user_id = null;
-	this.zone_column_count = null;
-	this.zone_line_count = null;
+    this.brush = null;
+    this.user_id = null;
+    this.zone_column_count = null;
+    this.zone_line_count = null;
 
-	// get session info from 
-	$.ajax({
-	    // FIXME: request with previous user_id
-	    url: SESSION_URL_JOIN + "?type=" + session_type,
-	    dataType: "json",
-	    type: 'GET',
-	    context: self,
-	    success: function( response ){
-		console.log('session/join response : ' + JSON.stringify(response) );
+    /**
+     * Semi-Constructor
+     */
+    var initialize = function() {
+        var user_id = $.cookie('user_id');
+        var username = $.cookie('username');
 
-		this.user_id = response.user_id;
-		this.username = response.username;
-		this.zone_column_count = response.zone_column_count;
-		this.zone_line_count = response.zone_line_count;
+        var session_url = SESSION_URL_JOIN + "?type=" + session_type;
+        if ( user_id != null ) {
+            session_url += "&user_id="+user_id;
+        }
+        if ( username != null ) {
+            session_url += "&username="+username;
+        }
 
-		console.log('session/join response mod : ' + JSON.stringify(this) );
+        // get session info from 
 
-		// FIXME: set cookie with user_id for next time
-		// FIXME: set username with username for next time
+        $.ajax({
+            url: session_url,
+            dataType: "json",
+            type: 'GET',
+            context: self,
+            success: function( response ){
+                console.log('session/join response : ' + JSON.stringify(response) );
 
-		callback( self );
-	    }
-	});
+                this.user_id = response.user_id;
+                this.username = response.username;
+                this.zone_column_count = response.zone_column_count;
+                this.zone_line_count = response.zone_line_count;
+
+                $.cookie( 'user_id', this.user_id );
+                $.cookie( 'username', this.username );
+                console.log('session/join response mod : ' + JSON.stringify(this) );
+
+                // FIXME: set cookie with user_id for next time
+                // FIXME: set username with username for next time
+
+                callback( self );
+            }
+        });
+    }
 
     this.to_s = function() { JSON.stringify(self); };
+
+
+    initialize();
 }
 
 
