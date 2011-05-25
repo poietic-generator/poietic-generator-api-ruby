29695d72515af4cb3cb3a968aff0ed606bee60e2
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2011-05-25 22:32:28 +0200

    database: Added zone property to user.

--- poietic-gen/database.rb
+++ poietic-gen/database.rb
@@ -7,6 +7,7 @@ class User
 	property :id,	Serial
 	property :session,	String, :required => true
 	property :name,	String, :required => true
+	property :zone, Integer, :required => true
 	property :created_at,	DateTime, :required => true
 	property :expires_at, DateTime, :required => true
 end
