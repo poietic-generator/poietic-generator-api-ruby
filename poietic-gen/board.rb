53ccb9ee1c830c577212651835634b130165d5fc
Glenn Y. Rolland <glenux@glenux.net>
2011-05-26 14:29:08 +0200

    board: added things coming from the old manager.

--- poietic-gen/board.rb
+++ poietic-gen/board.rb
@@ -31,5 +31,37 @@ module PoieticGen
 			zone = @zones[zone_idx]
 			zone.user = nil
 		end
+
+		#
+		# expend map creating new allocatable zones
+		#
+		def world_expand
+			# choose a side (using a spiral growth)
+			# expand that side
+			zone_past = @zones.last
+
+			zone_present = Zone.create_next zone_past
+			zone_future = Zone.create_next zone_past
+
+			# if the following collides, then keep the same vector
+			res = @zones.select do |zone_item|
+				( zone_item.position <=> zone_future ) == 0
+			end
+			unless res.empty? then
+				#collision with existing zone coordinates
+				zone_present.vector = zone_past.vector
+			end
+			@zones << zone_present
+		end
+
+		#
+		# reduce map removing unused zones from the border
+		#
+		def world_reduce
+			while true
+				zone = @zones.last
+				break if @zones.last
+			end
+		end
 	end
 end
