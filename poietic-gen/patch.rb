0fe7602b2eefe15ba3bd4c0d711247c3d7b61d5a
Glenn Y. Rolland <glenux@glenux.net>
2011-05-26 17:15:11 +0200

    patch: fixed typo in PoeticGen name (again).

--- poietic-gen/patch.rb
+++ poietic-gen/patch.rb
@@ -1,10 +1,11 @@
 
 require 'datamapper'
 
-module PoeticGen
+module PoieticGen
 
 	class Patch
 		include DataMapper::Resource
+
 		property :id,	Serial
 		property :color, String
 		property :coordinates, String
