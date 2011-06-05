334d2a76be5a8435f3fce4c532e1749e984a5d9f
Glenn Y. Rolland <glenux@glenux.net>
2011-06-05 14:55:35 +0200

    board: Insert allocation code for spiral (until external allocators...)

--- poietic-gen/board.rb
+++ poietic-gen/board.rb
@@ -1,67 +1,188 @@
 
+require 'poietic-gen/zone'
+
 module PoieticGen
 
+	#
+	# A class that manages the global drawing
+	#
 	class Board
-		@zones = [ ZONE_INIT ]
-		ZONE_INIT = []
+
+		private
+
+		# helper class fir direction
+		class V
+			attr_accessor :x, :y
+
+			def initialize x, y
+				@x, @y = x, y
+			end
+
+			def rotate_left!
+				@x, @y = -@y, @x
+				#puts "rotate! now %s" % self
+				self
+			end
+
+			def rotate_right!
+				@x, @y = -@y, @x
+				self
+			end
+
+			def + v
+				raise TypeError unless v.is_a? V
+				@x += v.x
+				@y += v.y
+				self
+			end
+
+			def - v
+				raise TypeError unless v.is_a? V
+				@x -= v.x
+				@y -= v.y
+				self
+			end
+
+			def == v
+				return false if v.x != @x
+				return false if v.y != @y
+				return true
+			end
+
+			def to_s
+				"(%s, %s)" % [ @x, @y ]
+			end
+
+			def to_a
+				[ @x, @y ]
+			end
+		end
 
 
 		# 
-		# allocates a zone for a new user
-		# uses the minimal index zone if it exists
-		# or creates one if necessary
-		# and return its index
+		# Find the first allocatable index
 		#
-		def allocate 
-			zone_result = nil
+		def _next_index 
+			result_index = nil
 
-			zones_free = @zones.select do |zone_item|
-				zone_item.user.nil?
+			# find a nil zone first
+			nil_zones = @zones.select{ |idx,zone| zone.nil? }
+			if nil_zones.size > 0 then
+				# got an unallocated zone !
+				result_index = nil_zones.first[0]
+			else
+				# try the normal method
+				result_index = @zones.size
 			end
-			if zones_free.empty? then
-				self.expand!
-			end
+			return result_index
+		end
 
-			zone = zones_free.first
-			zone.user = user_id
+
+		public
+
+		#
+		#
+		#
+		def initialize
+			# map index => Zone object (or nil if unallocated)
+			@zones = {}
+		end
+
+
+		#
+		# return index to position
+		#
+		def index_to_position idx
+			dir = V.new( 1, 0 )
+			pos = V.new( 0, 0 )
+			# puts "%s => %s" % [ 0, pos ]
+			idx.times do |cnt|
+				pos += dir
+				# puts "%s => %s" % [ cnt + 1, pos ]
+				if pos.x.abs == pos.y.abs then
+					unless ( pos.y <= 0 and pos.x == -pos.y ) then
+						dir.rotate_left!
+					end
+				elsif ( pos.y <= 0 and pos.x == -pos.y + 1 ) then
+					dir.rotate_left!
+				end
+			end
+			return pos.to_a
+		end
+
+
+		#
+		# return position from index
+		#
+		def position_to_index x, y
+			dir = V.new( 1, 0 )
+			pos = V.new( 0, 0 )
+			idx = 0
+			# puts "%s => %s" % [ 0, pos ]
+			while ( pos.x != x or pos.y != y ) do
+				idx += 1
+				pos += dir
+				# puts "%s => %s" % [ cnt + 1, pos ]
+				if pos.x.abs == pos.y.abs then
+					unless ( pos.y <= 0 and pos.x == -pos.y ) then
+						dir.rotate_left!
+					end
+				elsif ( pos.y <= 0 and pos.x == -pos.y + 1 ) then
+					dir.rotate_left!
+				end
+			end
+			return idx
+		end
+
+
+		#
+		# allocates and return 
+		# a zone		
+		#
+		def allocate user
+			next_index = _next_index()
+
+			zone = Zone.new self, next_index
+			zone.user = user
+
+			STDERR.puts "Allocation zone : ", zone.inspect
+
+			@zones[next_index] = zone
 			return zone
 		end
 
 		def free zone_idx
 			zone = @zones[zone_idx]
 			zone.user = nil
+			@zones[zone_idx]
 		end
 
-		#
-		# expend map creating new allocatable zones
-		#
-		def world_expand
-			# choose a side (using a spiral growth)
-			# expand that side
-			zone_past = @zones.last
-
-			zone_present = Zone.create_next zone_past
-			zone_future = Zone.create_next zone_past
-
-			# if the following collides, then keep the same vector
-			res = @zones.select do |zone_item|
-				( zone_item.position <=> zone_future ) == 0
-			end
-			unless res.empty? then
-				#collision with existing zone coordinates
-				zone_present.vector = zone_past.vector
-			end
-			@zones << zone_present
-		end
-
-		#
-		# reduce map removing unused zones from the border
-		#
-		def world_reduce
-			while true
-				zone = @zones.last
-				break if @zones.last
-			end
-		end
 	end
+
 end
+
+
+
+=begin
+test = {
+	0 => [0,0],
+	1 => [1,0],
+	2 => [1,1],
+	4 => [-1,1],
+	6 => [-1,-1],
+	9 => [2,-1]
+}
+
+test.each do |idx,pos|
+	posv = V.new( pos[0], pos[1] )
+	p = index_to_position idx
+	i = position_to_index pos[0], pos[1]
+
+	puts posv, idx
+	puts p, i
+
+	raise RuntimeError if p != posv
+	raise RuntimeError if i != idx
+end
+=end
+
