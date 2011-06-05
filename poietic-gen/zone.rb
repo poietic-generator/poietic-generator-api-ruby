1727440ed5cd5a0c6e195116356cda55d8eb1c78
Glenn Y. Rolland <glenux@glenux.net>
2011-06-05 21:54:35 +0200

    zone: apply received patch locally.

--- poietic-gen/zone.rb
+++ poietic-gen/zone.rb
@@ -6,11 +6,37 @@ module PoieticGen
 
 		attr_accessor :user
 
-		def initialize board, index
-			@board = board
+		def initialize index, position, width, height
 			@index = index
-			@position =	@board.index_to_position index
+			@position =	position
+			@width = width
+			@height = height
 			@user = nil
+
+			@data = []
+			@width.times do |w_cnt|
+				@data[w_cnt] = []
+				@height.times do |h_cnt|
+					@data[w_cnt][h_cnt] = nil
+				end
+			end
+		end
+
+		def apply drawing
+			# save patch into database
+			STDERR.puts "Zone - apply:"
+			return if drawing.nil?
+
+			drawing.each do |patch|
+				color = patch['color']
+				changes = patch['changes']
+				# FIXME: add patch into database
+				
+				changes.each do |x,y,t_offset|
+					@data[x][y] = color
+				end
+			end
+			
 		end
 	end
 end
