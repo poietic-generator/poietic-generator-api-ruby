7cfd3c8269380a346f0aa43bf7d873a4dabc8bf2
Glenn Y. Rolland <glenux@glenux.net>
2011-06-05 14:55:00 +0200

    zone: Removed all the allocation code from zone object.

--- poietic-gen/zone.rb
+++ poietic-gen/zone.rb
@@ -1,34 +1,16 @@
 
 module PoieticGen
 	class Zone
-		Struct :index, :position, :vector, :user
 
-		def initialize index, position, vector
+		attr_reader :index, :position
+
+		attr_accessor :user
+
+		def initialize board, index
+			@board = board
 			@index = index
-			@position = position
-			@vector = vector
+			@position =	@board.index_to_position index
 			@user = nil
 		end
-
-		def next_index
-			return (@index + 1)
-		end
-
-		def next_position
-			x = @position[0] + @vector[0]
-			y = @position[1] + @vector[1]
-			return [x,y]
-		end
-
-		def next_vector
-			v = [ - @vector[1], @vector[0] ]
-			return v
-		end
-
-		def self.create_next zone
-			return self.new zone.next_index, zone.next_position, zone.next_vector
-		end
-
 	end
-	ZONE_INIT = Zone.new 0, [0,0], [1,0]
 end
