028e3d46e6548a7f0f0a9ea04705eb7d7c0e9d14
Glenn Y. Rolland <glenux@glenux.net>
2011-06-05 14:54:33 +0200

    allocation spiral: Added pos_to_idx method.

--- poietic-gen/allocation/spiral.rb
+++ poietic-gen/allocation/spiral.rb
@@ -1,4 +1,5 @@
 
+# helper class fir direction
 class V
 	attr_accessor :x, :y
 
@@ -8,7 +9,7 @@ class V
 
 	def rotate_left!
 		@x, @y = -@y, @x
-		puts "rotate! now %s" % self
+		#puts "rotate! now %s" % self
 		self
 	end
 
@@ -31,11 +32,22 @@ class V
 		self
 	end
 
+	def == v
+		return false if v.x != @x
+		return false if v.y != @y
+		return true
+	end
+
 	def to_s
 		"(%s, %s)" % [ @x, @y ]
 	end
+
+	def to_a
+		[ @x, @y ]
+	end
 end
 
+# index to position
 def idx_to_pos idx
 	dir = V.new( 1, 0 )
 	pos = V.new( 0, 0 )
@@ -54,8 +66,49 @@ def idx_to_pos idx
 	return pos
 end
 
+# position from index
 def pos_to_idx x, y
+	dir = V.new( 1, 0 )
+	pos = V.new( 0, 0 )
+	idx = 0
+	# puts "%s => %s" % [ 0, pos ]
+	while ( pos.x != x or pos.y != y ) do
+		idx += 1
+		pos += dir
+		# puts "%s => %s" % [ cnt + 1, pos ]
+		if pos.x.abs == pos.y.abs then
+			unless ( pos.y <= 0 and pos.x == -pos.y ) then
+				dir.rotate_left!
+			end
+		elsif ( pos.y <= 0 and pos.x == -pos.y + 1 ) then
+			dir.rotate_left!
+		end
+	end
+	return idx
 	# nothing
 end
 
-idx_to_pos 0
+
+=begin
+test = {
+	0 => [0,0],
+	1 => [1,0],
+	2 => [1,1],
+	4 => [-1,1],
+	6 => [-1,-1],
+	9 => [2,-1]
+}
+
+test.each do |idx,pos|
+	posv = V.new( pos[0], pos[1] )
+	p = idx_to_pos idx
+	i = pos_to_idx pos[0], pos[1]
+
+	puts posv, idx
+	puts p, i
+
+	raise RuntimeError if p != posv
+	raise RuntimeError if i != idx
+end
+=end
+
