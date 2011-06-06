5bbd773c6a471f3e7f41d2742957152eee9facbb
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2011-06-07 00:24:08 +0200

    event: added helper methods create_join & to_hash.

--- poietic-gen/event.rb
+++ poietic-gen/event.rb
@@ -1,5 +1,6 @@
 
 require 'dm-core'
+require 'json'
 
 module PoieticGen
 	class Event
@@ -9,6 +10,26 @@ module PoieticGen
 		property :type,	String, :required => true
 		property :desc, String, :required => true
 		property :timestamp,	DateTime
+
+
+		def self.create_join uid, uzone
+			event = Event.create({ 
+				:type => 'join', 
+				:desc => JSON.generate({ :user => uid, :zone => uzone }),
+				:timestamp => DateTime.now
+			})
+			event.save
+		end
+
+		def to_hash
+			res = {
+				:id => self.id,
+				:type => self.type,
+				:desc => JSON.parse(self.desc),
+				:stamp => self.timestamp
+			}
+			return res
+		end
 	end
 
 end
