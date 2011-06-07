b46040e02aa4b43db3f26a99af09441257103067
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2011-06-07 21:40:26 +0200

    stroke: Added to_hash method.

--- poietic-gen/stroke.rb
+++ poietic-gen/stroke.rb
@@ -10,5 +10,14 @@ module PoieticGen
 		property :color, String, :required => true
 		property :changes, Text, :required => true, :lazy => false
 		property :timestamp,	DateTime
+
+		def to_hash
+			res = {
+				:color => self.color,
+				:changes => JSON.parse( self.changes ),
+				:stamp => self.timestamp
+			}
+			return res
+		end
 	end
 end
