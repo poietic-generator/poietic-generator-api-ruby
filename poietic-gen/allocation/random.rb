67c7fcfdd793b078a423e6af02f0b29c8b5cca51
Roland Laures <roland.laures@gnuside.com>
2011-06-07 14:32:04 +0200

    allocation: Added allocation strategies (spiral, random, generic).

---
+++ poietic-gen/allocation/random.rb
@@ -0,0 +1,38 @@
+
+module PoieticGen ; module Allocation
+
+	module Generic
+
+		#
+		# return a zone, somewhere...
+		#
+		def allocate
+			raise NotImplementedError
+		end
+
+		#
+		# free zone at given index
+		#
+		def free idx
+			raise NotImplementedError
+		end
+
+		#
+		# get position for given index
+		#
+		def idx_to_pos idx
+			raise NotImplementedError
+		end
+
+		# 
+		# get index for given position
+		# (or raise something if not allocated)
+		#
+		def pos_to_idx x, y
+			raise NotImplementedError
+		end
+
+	end
+
+end ; end
+
--- poietic-gen/allocation/spiral.rb
+++ poietic-gen/allocation/spiral.rb
@@ -1,92 +1,58 @@
 
-# helper class fir direction
-class V
-	attr_accessor :x, :y
+module PoieticGen ; module Allocation
+	module Spiral
 
-	def initialize x, y
-		@x, @y = x, y
-	end
+		# helper class fir direction
+		class V
+			attr_accessor :x, :y
 
-	def rotate_left!
-		@x, @y = -@y, @x
-		#puts "rotate! now %s" % self
-		self
-	end
-
-	def rotate_right!
-		@x, @y = -@y, @x
-		self
-	end
-
-	def + v
-		raise TypeError unless v.is_a? V
-		@x += v.x
-		@y += v.y
-		self
-	end
-
-	def - v
-		raise TypeError unless v.is_a? V
-		@x -= v.x
-		@y -= v.y
-		self
-	end
-
-	def == v
-		return false if v.x != @x
-		return false if v.y != @y
-		return true
-	end
-
-	def to_s
-		"(%s, %s)" % [ @x, @y ]
-	end
-
-	def to_a
-		[ @x, @y ]
-	end
-end
-
-# index to position
-def idx_to_pos idx
-	dir = V.new( 1, 0 )
-	pos = V.new( 0, 0 )
-	# puts "%s => %s" % [ 0, pos ]
-	idx.times do |cnt|
-		pos += dir
-		# puts "%s => %s" % [ cnt + 1, pos ]
-		if pos.x.abs == pos.y.abs then
-			unless ( pos.y <= 0 and pos.x == -pos.y ) then
-				dir.rotate_left!
+			def initialize x, y
+				@x, @y = x, y
 			end
-		elsif ( pos.y <= 0 and pos.x == -pos.y + 1 ) then
-			dir.rotate_left!
-		end
-	end
-	return pos
-end
 
-# position from index
-def pos_to_idx x, y
-	dir = V.new( 1, 0 )
-	pos = V.new( 0, 0 )
-	idx = 0
-	# puts "%s => %s" % [ 0, pos ]
-	while ( pos.x != x or pos.y != y ) do
-		idx += 1
-		pos += dir
-		# puts "%s => %s" % [ cnt + 1, pos ]
-		if pos.x.abs == pos.y.abs then
-			unless ( pos.y <= 0 and pos.x == -pos.y ) then
-				dir.rotate_left!
+			def rotate_left!
+				@x, @y = -@y, @x
+				#puts "rotate! now %s" % self
+				self
 			end
-		elsif ( pos.y <= 0 and pos.x == -pos.y + 1 ) then
-			dir.rotate_left!
+
+			def rotate_right!
+				@x, @y = -@y, @x
+				self
+			end
+
+			def + v
+				raise TypeError unless v.is_a? V
+				@x += v.x
+				@y += v.y
+				self
+			end
+
+			def - v
+				raise TypeError unless v.is_a? V
+				@x -= v.x
+				@y -= v.y
+				self
+			end
+
+			def == v
+				return false if v.x != @x
+				return false if v.y != @y
+				return true
+			end
+
+			def to_s
+				"(%s, %s)" % [ @x, @y ]
+			end
+
+			def to_a
+				[ @x, @y ]
+			end
 		end
-	end
-	return idx
-	# nothing
-end
+
+
+		def initialize
+		end
 
 
 =begin
@@ -111,4 +77,51 @@ test.each do |idx,pos|
 	raise RuntimeError if i != idx
 end
 =end
+
+
+		# index to position
+		def idx_to_pos idx
+			dir = V.new( 1, 0 )
+			pos = V.new( 0, 0 )
+			# puts "%s => %s" % [ 0, pos ]
+			idx.times do |cnt|
+				pos += dir
+				# puts "%s => %s" % [ cnt + 1, pos ]
+				if pos.x.abs == pos.y.abs then
+					unless ( pos.y <= 0 and pos.x == -pos.y ) then
+						dir.rotate_left!
+					end
+				elsif ( pos.y <= 0 and pos.x == -pos.y + 1 ) then
+					dir.rotate_left!
+				end
+			end
+			return pos
+		end
+
+		# position from index
+		def pos_to_idx x, y
+			dir = V.new( 1, 0 )
+			pos = V.new( 0, 0 )
+			idx = 0
+			# puts "%s => %s" % [ 0, pos ]
+			while ( pos.x != x or pos.y != y ) do
+				idx += 1
+				pos += dir
+				# puts "%s => %s" % [ cnt + 1, pos ]
+				if pos.x.abs == pos.y.abs then
+					unless ( pos.y <= 0 and pos.x == -pos.y ) then
+						dir.rotate_left!
+					end
+				elsif ( pos.y <= 0 and pos.x == -pos.y + 1 ) then
+					dir.rotate_left!
+				end
+			end
+			return idx
+			# nothing
+		end
+
+	end
+end
+
+
 
