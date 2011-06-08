a6dfbb1f2c8503d33cb70d86482e7602bc8edc3a
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2011-06-08 11:25:14 +0200

    chat: reorganize / beautify code.

--- static/js/chat.js
+++ static/js/chat.js
@@ -8,6 +8,9 @@ function Chat( p_session ) {
 
     this._queue = [];
 
+    /**
+     * Constructor
+     */
     this.initialize = function (p_session) {
         // register chat to session
         p_session.register(self);
@@ -16,7 +19,10 @@ function Chat( p_session ) {
         if (0 < p_session.other_users.length) {
             var select = $("#send-message-form-to");
             for (var i=0; i < p_session.other_users.length; i++) {
-                select.append('<option value="' + p_session.other_users[i].id + '">' + p_session.other_users[i].name + ' (' + p_session.other_users[i].id + ')</option>');
+                select.append('<option value="' 
+			+ p_session.other_users[i].id + '">' 
+			+ p_session.other_users[i].name 
+			+ ' (' + p_session.other_users[i].id + ')</option>');
             }
         }
 
@@ -24,9 +30,9 @@ function Chat( p_session ) {
         $("#send-message-form").submit(function(event){
             event.preventDefault();
 	    var message = {
-                user_dst: parseInt($(this).find("#send-message-form-to").val(), 10),
+                user_dst: parseInt($(self).find("#send-message-form-to").val(), 10),
                 stamp: new Date(),
-                content: $(this).find("#send-message-form-content").val()
+                content: $(self).find("#send-message-form-content").val()
             };
             self.queue_message( message );
 
@@ -35,26 +41,39 @@ function Chat( p_session ) {
         });
     };
 
-    this.get_messages = function () {
-        var queue   = this._queue;
-        this._queue = [];
-        return queue;
-    };
 
     this.queue_message = function (message) {
         this._queue.push(message);
     };
 
+
+    /**
+     *
+     */
+    this.get_messages = function () {
+        var queue   = self._queue;
+        self._queue = [];
+        return queue;
+    };
+
+
+    /**
+     *
+     */
     this.handle_event = function( ev ) {
 	// do nothing here :-)
+	console.log("chat/handle_event : %s", JSON.stringify( ev ));
     }
 
+
+    /**
+     *
+     */
     this.handle_message = function( msg ) {
+	// FIXME: do something here
+	console.log("chat/handle_message : %s", JSON.stringify( msg ));
     }
 
-    this.handle_stroke = function( stk ) {
-	// do nothing here :-)
-    }
 
     // call initialize method
     this.initialize( p_session );
