06f0ec950cf048849d91497041e9820bf13db2c6
Sebastian Schawohl <sebastian.schawohl@gnuside.com>
2011-06-08 16:16:59 +0200

    Initialize the color picker on startup but hide it, so the editor get the correct color.

--- static/js/page_draw.js
+++ static/js/page_draw.js
@@ -1,4 +1,4 @@
-var session = null, picker;
+var session = null;
 
 // instead of windows.onload
 $(document).ready( function() {
@@ -13,28 +13,24 @@ $(document).ready( function() {
 
             var board = new Board( session, 'session-board');
             var drawing = new Editor(session, board, 'session-drawing');
-            var chat = new Chat(session);
+            var chat = new Chat(session),
+            picker = new Color.Picker({
+                size: Math.floor($(window).width() / 3),
+                callback: function(hex) {
+                    drawing.color_set( "#" + hex );
+                }
+            });
+            $(picker.el).hide(); // hide color picker on startup
+            picker.el.style.bottom = "50px";
+            picker.el.style.left = "10px";
 
             //console.log("page_draw/ready: prepicker");
             $("#brush").click(function(event){
                 event.preventDefault();
-                var firstClick = false;
-                if (undefined === picker) {
-                    picker = new Color.Picker({
-                        size: Math.floor($(window).width() / 3),
-                        callback: function(hex) {
-                            drawing.color_set( "#" + hex );
-                        }
-                    });
-                    picker.el.style.bottom = "50px";
-                    picker.el.style.left = "10px";
-                    firstClick = true;
-                    $("#brush-action").text("Hide");
-                }
                 if (true === $(picker.el).is(":hidden")) {
                     $(picker.el).show();
                     $("#brush-action").text("Hide");
-                } else if (!firstClick) {
+                } else {
                     $(picker.el).hide();
                     $("#brush-action").text("Show");
                 }
