db74f4d2f4b2a7efe2cffa4093dd66a5e0e8dc1b
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2011-06-08 15:47:20 +0200

    js console: removed firebug dependency.

--- static/js/console.js
+++ static/js/console.js
@@ -2,7 +2,8 @@
 // vim: set ts=4 sw=4 et:
 "use strict";
 
-if (!("console" in window) || !("firebug" in console)) {
+if (!("console" in window)) {
+    alert("malbu");
     var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml", "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
     window.console = {};
     for (var i = 0, len = names.length; i < len; ++i) {
