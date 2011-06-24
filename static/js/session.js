9c1022e056e15a7410d49627e356c8ed90028df8
Glenn Y. Rolland <glenux@glenux.net>
2011-06-24 11:54:53 +0200

    Added headers.

--- static/js/session.js
+++ static/js/session.js
@@ -1,3 +1,24 @@
+/******************************************************************************/
+/*                                                                            */
+/*  Poetic Generator Reloaded is a multiplayer and collaborative art          */
+/*  experience.                                                               */
+/*                                                                            */
+/*  Copyright (C) 2011 - Gnuside                                              */
+/*                                                                            */
+/*  This program is free software: you can redistribute it and/or modify it   */
+/*  under the terms of the GNU Affero General Public License as published by  */
+/*  the Free Software Foundation, either version 3 of the License, or (at     */
+/*  your option) any later version.                                           */
+/*                                                                            */
+/*  This program is distributed in the hope that it will be useful, but       */
+/*  WITHOUT ANY WARRANTY; without even the implied warranty of                */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero  */
+/*  General Public License for more details.                                  */
+/*                                                                            */
+/*  You should have received a copy of the GNU Affero General Public License  */
+/*  along with this program.  If not, see <http://www.gnu.org/licenses/>.     */
+/*                                                                            */
+/******************************************************************************/
 
 
 // vim: set ts=4 sw=4 et:
