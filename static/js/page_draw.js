de377c970c79170c503efe39c9b7b61b3c2f3317
Sebastian Schawohl <sebastian.schawohl@gnuside.com>
2011-06-28 21:12:57 +0200

    Toggle color active state.

--- static/js/page_draw.js
+++ static/js/page_draw.js
@@ -56,6 +56,7 @@ $(document).ready( function() {
                     return false;
                 } else {
                     color_picker.show( this );
+                    $(this).addClass( "ui-btn-active" );
                     return true;
                 }
             });
