b8c023c3d491cca427dad46654514c39af8ccc22
Glenn Y. Rolland <glenux@glenux.net>
2011-07-05 23:20:33 +0200

    bot: Detect if environment requires a virtual X11.

--- bot/run.sh
+++ bot/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 COLOR=`cat /dev/urandom |head -c10 |md5sum |head -c6`
-phantomjs bot.js $COLOR
+COMMAND="phantomjs bot.js $COLOR"
+if [ -z "${DISPLAY:-}" ]; then
+	xvfb-run -a ${COMMAND}
+else
+	${COMMAND}
+fi
+
