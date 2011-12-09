5b22ebd46c736ff9700e84ca69aab1a1bb046b82
Glenn Y. Rolland <glenux@gmail.com>
2011-12-09 16:22:16 +0100

    run.sh : use thin instead of rackup.

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 
-LOGFILE=/var/log/poietic-$(date +%Y_%m_%d-%Hh%Mm%S).log
+LOGFILE=log/poietic-$(date +%Y_%m_%d-%Hh%Mm%S).log
 
 echo Logs are stored in : $LOGFILE
 
 cd `dirname $0`
 #rm -f *.sqlite3
-bundle exec rackup config.ru -o 0.0.0.0 -p 9393 | tee -a $LOGFILE
+#bundle exec rackup config.ru -o 0.0.0.0 -p 9393 | tee -a $LOGFILE
+
+bundle exec thin -C config/production/thin.yml -R config.ru start
 
 # For real production :
 #   thin -C thin-prod.yml -R config.ru start
