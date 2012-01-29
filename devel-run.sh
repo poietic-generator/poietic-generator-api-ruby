d1a6895e5fa97276ca9734773b8e4a4156e96302
Glenn Y. Rolland <glenux@gmail.com>
2012-01-29 17:59:56 +0100

    devel-run: add the debug flag.

--- devel-run.sh
+++ devel-run.sh
@@ -9,7 +9,7 @@ cd `dirname $0`
 #rm -f *.sqlite3
 #bundle exec rackup config.ru -o 0.0.0.0 -p 9393 | tee -a $LOGFILE
 
-bundle exec thin -C config/development/thin.yml -R config.ru start
+bundle exec thin --debug -C config/development/thin.yml -R config.ru start
 
 # For real production :
 #   thin -C thin-prod.yml -R config.ru start
