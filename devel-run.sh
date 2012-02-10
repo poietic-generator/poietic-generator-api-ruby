001a7eafb2beb317ca0b0beda5fb7569d88e2f6d
Glenn Y. Rolland <glenux@gmail.com>
2012-02-10 18:57:57 +0100

    devel-run: updated script to always run in foreground.

--- devel-run.sh
+++ devel-run.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
 
+trap 'devel_stop ' 2
 #LOGFILE=log/poietic-$(date +%Y_%m_%d-%Hh%Mm%S).log
 
 #echo Logs are stored in : $LOGFILE
 
-cd `dirname $0`
 #rm -f *.sqlite3
 #bundle exec rackup config.ru -o 0.0.0.0 -p 9393 | tee -a $LOGFILE
 
-bundle exec thin --debug -C config/development/thin.yml -R config.ru start
+devel_start(){
+	cd `dirname $0` ;
+	bundle exec thin --debug -C config/development/thin.yml -R config.ru start ;
+	tail -f log/thin.log ;
+}
+
+
+devel_stop(){
+	cd `dirname $0`
+	bundle exec thin --debug -C config/development/thin.yml -R config.ru stop
+}
+
+
+devel_start
+devel_stop
 
 # For real production :
 #   thin -C thin-prod.yml -R config.ru start
