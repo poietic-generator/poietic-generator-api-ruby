9f525e13806e3c00988d5b7e108b7c9c499b1ce3
@@@No user configured@@@ <@@@No user configured@@@>
2012-12-30 13:51:18 +0100

    zone.js: Partially jslinted. A probable bug remains.

--- static/js/zone.js
+++ static/js/zone.js
@@ -20,180 +20,191 @@
 /*                                                                            */
 /******************************************************************************/
 
-// vim: set ts=4 sw=4 et:
-"use strict";
+/*jslint nomen:true*/
+/*global window, noconsole*/
 
-var ZONE_BACKGROUND_COLOR = '#000';
+(function (window) {
+	// vim: set ts=4 sw=4 et:
+	"use strict";
 
-function Zone( p_session, p_index, p_position, p_width, p_height ) {
-    var console = noconsole;
-    var self = this;
+	var ZONE_BACKGROUND_COLOR = '#000';
 
-    // zone dimensions
-    this.index = p_index;
-    this.position = p_position;
-    this.width = p_width;
-    this.height = p_height;
+	function Zone(p_session, p_index, p_position, p_width, p_height) {
+		var console = noconsole,
+			self = this,
+			_session = p_session,
 
-    // color matrix (for maintaining state localy)
-    var _session = p_session;
-    var _matrix = [];
+			//color matrix (for maintaining state localy)
+			_matrix = [],
 
-    // patches to send
-    var _output_queue = [];
+			// patches to send
+			_output_queue = [],
 
-    // the patch we are working on
-    var _current_patch = null;
+			// the patch we are working on
+			_current_patch = null,
 
-    // patches to apply localy
-    var _input_queue = [];
-
-    // console.log("zone/initialize width = %s", this.width );
-    // console.log("zone/initialize height = %s", this.height );
-
-    /**
-     * Set matrix default color
-     */
-    this.matrix_initialize = function() {
-        var imax = self.width * self.height;
-        for (var i=0; i<imax; i++){
-            _matrix[i] = ZONE_BACKGROUND_COLOR;
-        }
-    }
+			// patches to apply localy
+			_input_queue = [];
 
 
-    /*
-     * Set matrix position to given color
-     */
-    this.pixel_set = function( pos, color ) {
-        var idx = Math.floor( pos.y ) * self.width + Math.floor(pos.x);
-        // console.log("zone/pixel_set idx = %s", idx );
-        _matrix[idx] = color;
-    };
+		// zone dimensions
+		this.index = p_index;
+		this.position = p_position;
+		this.width = p_width;
+		this.height = p_height;
 
 
-    /*
-     * Get color at given matrix position
-     */
-    this.pixel_get = function( pos ) {
-        var idx = Math.floor( pos.y ) * self.width + Math.floor( pos.x );
-        // console.log("zone/pixel_get idx = %s", idx );
-        return _matrix[idx];
-    };
+		// console.log("zone/initialize width = %s", this.width );
+		// console.log("zone/initialize height = %s", this.height );
+
+		/**
+		 * Set matrix default color
+		 */
+		this.matrix_initialize = function () {
+			var imax = self.width * self.height,
+				i;
+			for (i = 0; i < imax; i += 1) {
+				_matrix[i] = ZONE_BACKGROUND_COLOR;
+			}
+		};
 
 
-    /*
-     * Return whethe given position is inside or outside zone
-     */
-    this.contains_position = function( pos ) {
-        if ( pos.x < 0 ) return false;
-        if ( pos.x >= self.width ) return false;
-        if ( pos.y < 0 ) return false;
-        if ( pos.y >= self.height ) return false;
-        return true;
-    }
+		/*
+		 * Set matrix position to given color
+		 */
+		this.pixel_set = function (pos, color) {
+			var idx = Math.floor(pos.y) * self.width + Math.floor(pos.x);
+			// console.log("zone/pixel_set idx = %s", idx );
+			_matrix[idx] = color;
+		};
 
 
-    /**
-     * Create patch out of latest changes
-     */
-    this.patch_record = function( pos, color ) {
-        var patch_update;
-        var patch_enqueue;
+		/*
+		 * Get color at given matrix position
+		 */
+		this.pixel_get = function (pos) {
+			var idx = Math.floor(pos.y) * self.width + Math.floor(pos.x);
+			// console.log("zone/pixel_get idx = %s", idx );
+			return _matrix[idx];
+		};
 
 
-        if ( _current_patch == null ) {
-            // console.log("zone/patch_record: patch creation!");
-            _current_patch = {
-                zone: self.index,
-                diff: ((new Date()) - _session.last_update_time)/1000,
-                color: color,
-                changes: [ [ pos.x, pos.y, 0 ]  ]
-            }
-        } else {
-            if ( _current_patch.color != color) {
-                self.patch_enqueue();
-                _current_patch = {
-                    zone: self.index,
-                    diff: (new Date()) - self._session._last_update_time,
-                    color: color,
-                    changes: [ [ pos.x, pos.y, 0 ]  ]
-                }
-            } else {
-                // console.log("zone/patch_record: patch update!");
-
-                // CONSTRAINT : we drop duplicate coordinates from a single patch if latest record it the same
-                var prev_record = null;
-                if ( _current_patch.changes.length > 0 ) {
-                    prev_record = _current_patch.changes[ _current_patch.changes.length - 1 ];
-                }
-                if ( ( prev_record != null ) && ( prev_record[0] == pos.x ) && ( prev_record[1] == pos.y ) ) {
-                    // drop
-                } else {
-                    _current_patch.changes.push( [ pos.x, pos.y, 0 ] );
-                }
-            }
-        }
-        // console.log( "zone/patch_record: _current_patch = %s", JSON.stringify( _current_patch ) );
-    };
+		/*
+		 * Return whethe given position is inside or outside zone
+		 */
+		this.contains_position = function (pos) {
+			if (pos.x < 0) { return false; }
+			if (pos.x >= self.width) { return false; }
+			if (pos.y < 0) { return false; }
+			if (pos.y >= self.height) { return false; }
+			return true;
+		};
 
 
-    /**
-     * Push current patch to local patch queue
-     */
-    this.patch_enqueue = function() {
-        //console.log("zone/patch_enqueue: !");
-        if ( _current_patch != null ) {
-            _output_queue.push(_current_patch);
-            _session.dispatch_strokes( [ _current_patch ] );
-            _current_patch = null;
-            console.log("zone/patch_enqueue: output queue = %s", JSON.stringify( _output_queue ) );
-        }
-    };
+		/**
+		 * Create patch out of latest changes
+		 */
+		this.patch_record = function (pos, color) {
+			var patch_update,
+				patch_enqueue,
+				prev_record = null;
+
+			if (_current_patch === null) {
+				// console.log("zone/patch_record: patch creation!");
+				_current_patch = {
+					zone: self.index,
+					diff: ((new Date()) - _session.last_update_time) / 1000,
+					color: color,
+					changes: [ [ pos.x, pos.y, 0 ]  ]
+				};
+			} else {
+				if (_current_patch.color !== color) {
+					self.patch_enqueue();
+					_current_patch = {
+						zone: self.index,
+						diff: (new Date()) - self._session._last_update_time,
+						color: color,
+						changes: [ [ pos.x, pos.y, 0 ]  ]
+					};
+				} else {
+					// console.log("zone/patch_record: patch update!");
+
+					// CONSTRAINT : we drop duplicate coordinates from a single patch if latest record it the same
+					if (_current_patch.changes.length > 0) {
+						prev_record = _current_patch.changes[_current_patch.changes.length - 1];
+					}
+					if ((prev_record !== null) && (prev_record[0] === pos.x)
+							&& (prev_record[1] === pos.y)) {
+						// drop
+					} else {
+						_current_patch.changes.push([ pos.x, pos.y, 0 ]);
+					}
+				}
+			}
+			// console.log( "zone/patch_record: _current_patch = %s", JSON.stringify( _current_patch ) );
+		};
 
 
-    /**
-      *
-      */
-    this.patches_get = function() {
-        var aggregate = [];
-
-        while ( _output_queue.length > 0 ) {
-            // FIXME: compute relative time since last sync
-            aggregate.push( _output_queue.shift() );
-        }
-
-        return aggregate;
-    }
-
-
-    /**
-      * Request application for a set of patches to current zone
-      */
-    this.patch_apply = function( p_patch ) {
-        var color = p_patch.color;
-        var cgset = null;
-        var zone_pos = null;
-
-        for ( var i=0; i<p_patch.changes.length; i++ ) {
-            cgset = p_patch.changes[i];
-            zone_pos = { x: cgset[0], y: cgset[1] }
-            self.pixel_set( zone_pos, color );
-        }
-    }
+		/**
+		 * Push current patch to local patch queue
+		 */
+		this.patch_enqueue = function () {
+			//console.log("zone/patch_enqueue: !");
+			if (_current_patch !== null) {
+				_output_queue.push(_current_patch);
+				_session.dispatch_strokes([ _current_patch ]);
+				_current_patch = null;
+				console.log("zone/patch_enqueue: output queue = %s", JSON.stringify(_output_queue));
+			}
+		};
 
 
-    /**
-     *
-     */
-    this.patches_push = function( p_patches ) {
-        // FIXME: call apply patch for each patches' relative time
-        // FIXME : append aggregate  instead of replacing
-        _input_queue = p_aggregate;
-    }
+		/**
+		 *
+		 */
+		this.patches_get = function () {
+			var aggregate = [];
 
-    // constructor
-    self.matrix_initialize();
+			while (_output_queue.length > 0) {
+				// FIXME: compute relative time since last sync
+				aggregate.push(_output_queue.shift());
+			}
 
-}
+			return aggregate;
+		};
+
+
+		/**
+		 * Request application for a set of patches to current zone
+		 */
+		this.patch_apply = function (p_patch) {
+			var color = p_patch.color,
+				cgset = null,
+				zone_pos = null,
+				i;
+
+			for (i = 0; i < p_patch.changes.length; i += 1) {
+				cgset = p_patch.changes[i];
+				zone_pos = { x: cgset[0], y: cgset[1] };
+				self.pixel_set(zone_pos, color);
+			}
+		};
+
+
+		/**
+		 *
+		 */
+		this.patches_push = function (p_patches) {
+			// FIXME: call apply patch for each patches' relative time
+			// FIXME : append aggregate  instead of replacing
+			_input_queue = p_aggregate;
+		};
+
+		// constructor
+		self.matrix_initialize();
+	}
+
+	window.ZONE_BACKGROUND_COLOR = ZONE_BACKGROUND_COLOR;
+
+}(window));
 
