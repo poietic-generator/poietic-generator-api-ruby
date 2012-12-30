5e1da0f8f9d8edcc75960fb8a8d1c8bb510143f8
@@@No user configured@@@ <@@@No user configured@@@>
2012-12-30 14:11:04 +0100

    view_session: jslinted code. Scope objects not exposed yet.

--- static/js/view_session.js
+++ static/js/view_session.js
@@ -20,250 +20,261 @@
 /*                                                                            */
 /******************************************************************************/
 
+/*jslint nomen:true*/
+/*global document, window, noconsole, jQuery*/
 
 // vim: set ts=4 sw=4 et:
-"use strict";
 
-var VIEW_SESSION_URL_JOIN = "/api/session/snapshot";
-var VIEW_SESSION_URL_UPDATE = "/api/session/play";
+(function (document, window, $) {
+	"use strict";
 
-var VIEW_SESSION_UPDATE_INTERVAL = 10000 ;
-var VIEW_PLAY_UPDATE_INTERVAL = VIEW_SESSION_UPDATE_INTERVAL / 1000 ;
+	var VIEW_SESSION_URL_JOIN = "/api/session/snapshot",
+		VIEW_SESSION_URL_UPDATE = "/api/session/play",
 
-var STATUS_INFORMATION = 1
-var STATUS_SUCCESS = 2
-var STATUS_REDIRECTION = 3
-var STATUS_SERVER_ERROR = 4
-var STATUS_BAD_REQUEST = 5
+		VIEW_SESSION_UPDATE_INTERVAL = 10000,
+		VIEW_PLAY_UPDATE_INTERVAL = VIEW_SESSION_UPDATE_INTERVAL / 1000,
 
-
-
-function ViewSession( callback ) {
-    var console = noconsole;
-
-    var self = this,
-    _observers = null,
-    _start_date = 0,
-    _duration = 0,
-    _timer = null,
-    _restart = false,
-    _play_speed = 1;
+		STATUS_INFORMATION = 1,
+		STATUS_SUCCESS = 2,
+		STATUS_REDIRECTION = 3,
+		STATUS_SERVER_ERROR = 4,
+		STATUS_BAD_REQUEST = 5;
 
 
-    this.zone_column_count = null;
-    this.zone_line_count = null;
+	function ViewSession(callback) {
+		var console = noconsole,
+			self = this,
+			_observers = null,
+			_start_date = 0,
+			_duration = 0,
+			_timer = null,
+			_restart = false,
+			_play_speed = 1;
 
 
-    /**
-     * Semi-Constructor
-     */
-    this.initialize = function() {
-
-        _observers = [];
-        var date = (_restart) ? 0 : -1;
-
-        // get session info from
-        $.ajax({
-            url: VIEW_SESSION_URL_JOIN,
-            data: {
-                date: date,
-                session: "default"
-            },
-            dataType: "json",
-            type: 'GET',
-            context: self,
-            success: function( response ){
-                console.log('session/join response : ' + JSON.stringify(response) );
-
-                this.zone_column_count = response.zone_column_count;
-                this.zone_line_count = response.zone_line_count;
-
-                _start_date = response.start_date;
-                if (!_restart) {
-                    _duration = response.duration;
-                } else {
-                    _duration = 0;
-                }
-                // console.log('session/join response mod : ' + JSON.stringify(this) );
-
-                self.other_zones = response.zones;
-
-                callback( self );
-
-                //console.log('session/join post-callback ! observers = %s', JSON.stringify( _observers ));
-                // handle other zone events
-                for (var i=0;i<self.other_zones.length;i++) {
-                    console.log('session/join on zone %s',JSON.stringify(self.other_zones[i]));
-                    self.dispatch_strokes( self.other_zones[i].content );
-                }
-
-                self.setTimer( self.update, 1 );
-
-                console.log('session/join end');
-            }
-        });
-    };
+		this.zone_column_count = null;
+		this.zone_line_count = null;
 
 
-    /**
-     * Treat not ok Status (!STATUS_SUCCESS)
-     */
-    this.treat_status_nok = function ( response ) {
+		/**
+		 * Semi-Constructor
+		 */
+		this.initialize = function () {
 
-        switch(response.status[0]) {
-            case STATUS_INFORMATION:
-                break;
-            case STATUS_SUCCESS:
-                // ???
-                break;
-            case STATUS_REDIRECTION:
-                // We got redirected for some reason, we do execute ourselfs
-                console.log("STATUS_REDIRECTION --> Got redirected to '%s'" % response.status[2]);
-                document.location.href = response.status[2];
-                break;
-            case STATUS_SERVER_ERROR:
-                // FIXME : We got a server error, we should try to reload the page.
-                break;
-            case STATUS_BAD_REQUEST:
-                // FIXME : OK ???
-                break;
-        }
-        return null;
-    };
+			_observers = [];
+			var date = (_restart) ? 0 : -1;
 
+			// get session info from
+			$.ajax({
+				url: VIEW_SESSION_URL_JOIN,
+				data: {
+					date: date,
+					session: "default"
+				},
+				dataType: "json",
+				type: 'GET',
+				context: self,
+				success: function (response) {
+					var i;
 
-    /**
-     *
-     */
-    this.update = function(){
+					console.log('session/join response : ' + JSON.stringify(response));
 
-        var strokes_updates = [], req;
+					this.zone_column_count = response.zone_column_count;
+					this.zone_line_count = response.zone_line_count;
 
-        // assign real values if objets are present
-        if (_observers.length < 1) {
-            self.setTimer( self.update, VIEW_SESSION_UPDATE_INTERVAL );
-            return null;
-        }
+					_start_date = response.start_date;
+					if (!_restart) {
+						_duration = response.duration;
+					} else {
+						_duration = 0;
+					}
+					// console.log('session/join response mod : ' + JSON.stringify(this) );
 
-        strokes_updates = []
-        for (var i=0; i<_observers.length; i++){
-            if (_observers[i].get_strokes) {
-                strokes_updates = strokes_updates.concat( strokes_updates, _observers[i].get_strokes() );
-            }
-        }
+					self.other_zones = response.zones;
 
-        console.log("session/update: strokes_updates = %s", JSON.stringify( strokes_updates ));
+					callback(self);
 
-        req = {
-            session: "default",
-            since: _start_date + _duration,
-            duration: VIEW_PLAY_UPDATE_INTERVAL * _play_speed
-        };
+					//console.log('session/join post-callback ! observers = %s', JSON.stringify( _observers ));
+					// handle other zone events
+					for (i = 0; i < self.other_zones.length; i += 1) {
+						console.log('session/join on zone %s', JSON.stringify(self.other_zones[i]));
+						self.dispatch_strokes(self.other_zones[i].content);
+					}
 
-        console.log("session/update: req = %s", JSON.stringify( req ) );
-        $.ajax({
-            url: VIEW_SESSION_URL_UPDATE,
-            dataType: "json",
-            data: req,
-            type: 'GET',
-            context: self,
-            success: function( response ) {
-                console.log('session/update response : ' + JSON.stringify( response ) );
-                self.treat_status_nok(response);
-                if (response.status[0] != STATUS_SUCCESS) {
-                    self.setTimer( self.update, VIEW_SESSION_UPDATE_INTERVAL * 2 );
-                }
+					self.setTimer(self.update, 1);
 
-                if (!_restart) {
-                    _duration = response.duration;
-                } else {
-                    _duration+= VIEW_PLAY_UPDATE_INTERVAL * _play_speed;
-                    if ( response.duration < _duration  ) {
-                        _duration = response.duration;
-                        _restart = false;
-                    }
-                }
-
-                self.dispatch_events( response.events );
-                self.dispatch_strokes( response.strokes );
-
-                self.setTimer( self.update, VIEW_SESSION_UPDATE_INTERVAL );
-            },
-            error: function( response ) {
-               self.setTimer( self.update, VIEW_SESSION_UPDATE_INTERVAL * 2 );
-           }
-        });
-
-    };
+					console.log('session/join end');
+				}
+			});
+		};
 
 
-    this.dispatch_events = function( events ){
-        for (var i=0; i<events.length; i++) {
-            for (var o=0; o<_observers.length;o++){
-                if (_observers[o].handle_event) {
-                    _observers[o].handle_event( events[i] );
-                }
-            }
-        }
-    }
-
-    this.dispatch_strokes = function( strokes ){
-        for (var i=0; i<strokes.length; i++) {
-            for (var o=0; o<_observers.length;o++){
-                if (_observers[o].handle_stroke) {
-                    _observers[o].handle_stroke( strokes[i] );
-                }
-            }
-        }
-    };
-
-    this.dispatch_reset = function () {
-        for (var o=0; o<_observers.length; o++){
-            if (_observers[o].handle_reset) {
-                _observers[o].handle_reset();
-            }
-        }
-    };
-
-    this.register = function( p_observer ){
-        _observers.push( p_observer );
-    };
+		/**
+		 * Treat not ok Status (!STATUS_SUCCESS)
+		 */
+		this.treat_status_nok = function (response) {
+			switch (response.status[0]) {
+			case STATUS_INFORMATION:
+				break;
+			case STATUS_SUCCESS:
+				// ???
+				break;
+			case STATUS_REDIRECTION:
+				// We got redirected for some reason, we do execute ourselfs
+				console.log("STATUS_REDIRECTION --> Got redirected to :" + response.status[2]);
+				document.location.href = response.status[2];
+				break;
+			case STATUS_SERVER_ERROR:
+				// FIXME : We got a server error, we should try to reload the page.
+				break;
+			case STATUS_BAD_REQUEST:
+				// FIXME : OK ???
+				break;
+			}
+			return null;
+		};
 
 
-    this.clearTimer = function () {
-        if (null !== _timer) {
-            window.clearTimeout( _timer );
-            _timer = null;
-        }
-    };
+		/**
+		 *
+		 */
+		this.update = function () {
 
-    this.setTimer = function ( fn, interval ) {
-        this.clearTimer();
-        _timer = window.setTimeout( fn, interval );
-    };
+			var strokes_updates = [], req, i;
+
+			// assign real values if objets are present
+			if (_observers.length < 1) {
+				self.setTimer(self.update, VIEW_SESSION_UPDATE_INTERVAL);
+				return null;
+			}
+
+			strokes_updates = [];
+			for (i = 0; i < _observers.length; i += 1) {
+				if (_observers[i].get_strokes) {
+					strokes_updates = strokes_updates.concat(strokes_updates, _observers[i].get_strokes());
+				}
+			}
+
+			console.log("session/update: strokes_updates = " + JSON.stringify(strokes_updates));
+
+			req = {
+				session: "default",
+				since: _start_date + _duration,
+				duration: VIEW_PLAY_UPDATE_INTERVAL * _play_speed
+			};
+
+			console.log("session/update: req = " + JSON.stringify(req));
+			$.ajax({
+				url: VIEW_SESSION_URL_UPDATE,
+				dataType: "json",
+				data: req,
+				type: 'GET',
+				context: self,
+				success: function (response) {
+					console.log('session/update response : ' + JSON.stringify(response));
+					self.treat_status_nok(response);
+					if (response.status[0] !== STATUS_SUCCESS) {
+						self.setTimer(self.update, VIEW_SESSION_UPDATE_INTERVAL * 2);
+					}
+
+					if (!_restart) {
+						_duration = response.duration;
+					} else {
+						_duration += VIEW_PLAY_UPDATE_INTERVAL * _play_speed;
+						if (response.duration < _duration) {
+							_duration = response.duration;
+							_restart = false;
+						}
+					}
+
+					self.dispatch_events(response.events);
+					self.dispatch_strokes(response.strokes);
+
+					self.setTimer(self.update, VIEW_SESSION_UPDATE_INTERVAL);
+				},
+				error: function (response) {
+					self.setTimer(self.update, VIEW_SESSION_UPDATE_INTERVAL * 2);
+				}
+			});
+
+		};
 
 
-    /**
-     * Play from current position
-     */
-    this.current = function () {
-        console.log("view_session/current");
-        _restart = false;
-        this.clearTimer();
-        this.initialize();
-        this.dispatch_reset( this );
-    };
+		this.dispatch_events = function (events) {
+			var i, o;
+			for (i = 0; i < events.length; i += 1) {
+				for (o = 0; o < _observers.length; o += 1) {
+					if (_observers[o].handle_event) {
+						_observers[o].handle_event(events[i]);
+					}
+				}
+			}
+		};
 
-    /**
-     * Replay from beginning
-     */
-    this.restart = function () {
-        console.log("view_session/restart");
-        _restart = true;
-        this.clearTimer();
-        this.initialize();
-        this.dispatch_reset( this );
-    };
+		this.dispatch_strokes = function (strokes) {
+			var i, o;
+			for (i = 0; i < strokes.length; i += 1) {
+				for (o = 0; o < _observers.length; o += 1) {
+					if (_observers[o].handle_stroke) {
+						_observers[o].handle_stroke(strokes[i]);
+					}
+				}
+			}
+		};
 
-    this.initialize();
-}
+		this.dispatch_reset = function () {
+			var o;
+			for (o = 0; o < _observers.length; o += 1) {
+				if (_observers[o].handle_reset) {
+					_observers[o].handle_reset();
+				}
+			}
+		};
+
+		this.register = function (p_observer) {
+			_observers.push(p_observer);
+		};
+
+
+		this.clearTimer = function () {
+			if (null !== _timer) {
+				window.clearTimeout(_timer);
+				_timer = null;
+			}
+		};
+
+		this.setTimer = function (fn, interval) {
+			this.clearTimer();
+			_timer = window.setTimeout(fn, interval);
+		};
+
+
+		/**
+		 * Play from current position
+		 */
+		this.current = function () {
+			console.log("view_session/current");
+			_restart = false;
+			this.clearTimer();
+			this.initialize();
+			this.dispatch_reset(this);
+		};
+
+		/**
+		 * Replay from beginning
+		 */
+		this.restart = function () {
+			console.log("view_session/restart");
+			_restart = true;
+			this.clearTimer();
+			this.initialize();
+			this.dispatch_reset(this);
+		};
+
+		this.initialize();
+	}
+
+	//FIXME: expose scope objects
+	
+}(document, window, jQuery));
+
