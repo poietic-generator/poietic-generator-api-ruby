0fe566c3c8aa60e32e28e6423b1ead9c5a30a9ca
Glenn Y. Rolland <glenux@glenux.net>
2013-01-03 10:13:10 +0100

    static/js board: Finish jslinting.

--- static/js/board.js
+++ static/js/board.js
@@ -21,138 +21,144 @@
 /******************************************************************************/
 
 /*jslint browser: true*/
-/*global $, jQuery, document, Zone */
+/*global $, jQuery, document, Zone, console */
 
-"use strict";
+// FIXME expose board objects to window
 
-/**
- * Global view
- */
-function Board(p_session) {
-	//var console = window.noconsole;
+(function () {
 
-	var self = this,
-		realCanvas,
-		context,
-		zones,
-		users,
-		session;
-
-    this.name = "Board";
-
-
+	"use strict";
 
 	/**
-	 * Constructor
-	 */
-	this.initialize = function (p_session) {
+	* Global view
+	*/
+	function Board(p_session) {
+		//var console = window.noconsole;
 
-		// loop vars
-		var i, z;
+		var self = this,
+			realCanvas,
+			context,
+			zones,
+			users,
+			session;
 
-		session = p_session;
-		session.register(self);
-		zones = {};
-		users = {};
-
-		// fill zones with zones from session
-		if (undefined !== session.user_zone) {
-			zones[session.user_zone.index] = new Zone(
-				session,
-				session.user_zone.index,
-				session.user_zone.position,
-				session.zone_column_count,
-				session.zone_line_count
-			);
-		}
-
-		for (i = 0; i < p_session.other_zones.length; i += 1) {
-			z = p_session.other_zones[i];
-
-			zones[z.index] = new Zone(
-				session,
-				z.index,
-				z.position,
-				session.zone_column_count,
-				session.zone_line_count
-			);
-		}
-		console.log("board/initialize: zones = %s", JSON.stringify(zones));
-	};
+		this.name = "Board";
 
 
-	/**
-	 *
-	 */
-	this.handle_event = function (ev) {
-		var z;
-		console.log("board/handle_event : %s", JSON.stringify(ev));
-		if (ev.type === 'join') {
-			z = ev.desc.zone;
-			zones[z.index] = new Zone(
-				session,
-				z.index,
-				z.position,
-				session.zone_column_count,
-				session.zone_line_count
-			);
-		} else if (ev.type === 'leave') {
-			z = ev.desc.zone;
-			console.log("board/handle_event: _zones bf delete %s", JSON.stringify(zones));
-			delete zones[z.index];
-		} else {
-			console.log("board/handle_event: unknown event");
-		}
-	};
 
+		/**
+		* Constructor
+		*/
+		this.initialize = function (p_session) {
 
-	/**
-	 *
-	 */
-	this.get_zone = function (index) {
-		// console.log("board/get_zone(%s) : %s", index, JSON.stringify( _zones[index] ) );
-		return zones[index];
-	};
+			// loop vars
+			var i, z;
 
+			session = p_session;
+			session.register(self);
+			zones = {};
+			users = {};
 
-	/**
-	 * Return the list of existing zone ids
-	 */
-	this.get_zone_list = function () {
-		var keys = [],
-			i;
-
-		for (i in zones) {
-			if (zones.hasOwnProperty(i)) {
-				keys.push(parseInt(i, 10));
+			// fill zones with zones from session
+			if (undefined !== session.user_zone) {
+				zones[session.user_zone.index] = new Zone(
+					session,
+					session.user_zone.index,
+					session.user_zone.position,
+					session.zone_column_count,
+					session.zone_line_count
+				);
 			}
-		}
-		// console.log("board/get_zone_list : %s", JSON.stringify( keys ));
-		return keys;
-	};
+
+			for (i = 0; i < p_session.other_zones.length; i += 1) {
+				z = p_session.other_zones[i];
+
+				zones[z.index] = new Zone(
+					session,
+					z.index,
+					z.position,
+					session.zone_column_count,
+					session.zone_line_count
+				);
+			}
+			console.log("board/initialize: zones = %s", JSON.stringify(zones));
+		};
 
 
-	/**
-	 *
-	 */
-	this.handle_stroke = function (stk) {
-		console.log("board/handle_stroke : stroke = %s", JSON.stringify(stk));
-		console.log("board/handle_stroke : zones = %s", JSON.stringify(zones));
-		var z = zones[stk.zone];
-		if (z) {
-			z.patch_apply(stk);
-		} else {
-			console.warn("board/handle_stroke: trying to apply stroke for missing zone %s", stk.zone);
-		}
-	};
+		/**
+		*
+		*/
+		this.handle_event = function (ev) {
+			var z;
+			console.log("board/handle_event : " + JSON.stringify(ev));
+			if (ev.type === 'join') {
+				z = ev.desc.zone;
+				zones[z.index] = new Zone(
+					session,
+					z.index,
+					z.position,
+					session.zone_column_count,
+					session.zone_line_count
+				);
+			} else if (ev.type === 'leave') {
+				z = ev.desc.zone;
+				console.log("board/handle_event: _zones bf delete %s", JSON.stringify(zones));
+				delete zones[z.index];
+			} else {
+				console.log("board/handle_event: unknown event");
+			}
+		};
 
 
-	this.handle_reset = function (session) {
-		console.log("board/handle_reset");
-		this.initialize(session);
-	};
+		/**
+		*
+		*/
+		this.get_zone = function (index) {
+			// console.log("board/get_zone(%s) : %s", index, JSON.stringify( _zones[index] ) );
+			return zones[index];
+		};
 
 
-	this.initialize(p_session);
-}
+		/**
+		* Return the list of existing zone ids
+		*/
+		this.get_zone_list = function () {
+			var keys = [],
+				i;
+
+			for (i in zones) {
+				if (zones.hasOwnProperty(i)) {
+					keys.push(parseInt(i, 10));
+				}
+			}
+			// console.log("board/get_zone_list : %s", JSON.stringify( keys ));
+			return keys;
+		};
+
+
+		/**
+		*
+		*/
+		this.handle_stroke = function (stk) {
+			console.log("board/handle_stroke : stroke = %s", JSON.stringify(stk));
+			console.log("board/handle_stroke : zones = %s", JSON.stringify(zones));
+			var z = zones[stk.zone];
+			if (z) {
+				z.patch_apply(stk);
+			} else {
+				console.warn("board/handle_stroke: trying to apply stroke for missing zone %s", stk.zone);
+			}
+		};
+
+
+		this.handle_reset = function (session) {
+			console.log("board/handle_reset");
+			this.initialize(session);
+		};
+
+
+		this.initialize(p_session);
+	}
+
+}());
 
