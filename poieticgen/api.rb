0d9c8cd1848f02d4e869b9ea940f74a11da2db57
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2013-05-09 15:34:19 +0200

    api: enable huge debug.

--- poieticgen/api.rb
+++ poieticgen/api.rb
@@ -97,7 +97,8 @@ module PoieticGen
 
 			set :config, config
 			set :manager, (PoieticGen::Manager.new config)
-			DataMapper::Logger.new(STDERR, :info)
+			#DataMapper::Logger.new(STDERR, :info)
+			DataMapper::Logger.new(STDERR, :debug)
 			hash = config.database.get_hash
 			pp "db hash :", hash
 			DataMapper.setup(:default, hash)
