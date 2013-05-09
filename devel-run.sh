59d7db16c0c862547e8962a03fbcae9d2d1dce0c
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2013-05-09 20:27:06 +0200

    Use unicorn for devel too.

--- devel-run.sh
+++ devel-run.sh
@@ -9,24 +9,34 @@ trap 'devel_stop ' 2
 #rm -f *.sqlite3
 #bundle exec rackup config.ru -o 0.0.0.0 -p 9393 | tee -a $LOGFILE
 
-devel_start(){
-	cd `dirname $0` ;
-	bundle exec thin --debug -C config/development/thin.yml -R config.ru start ;
-	tail -f log/thin.log ;
-}
+#devel_start(){
+#	cd `dirname $0` ;
+#	bundle exec thin --debug -C config/development/thin.yml -R config.ru start ;
+#	tail -f log/thin.log ;
+#}
 
 
-devel_stop(){
-	cd `dirname $0`
-	bundle exec thin --debug -C config/development/thin.yml -R config.ru stop
-}
+#devel_stop(){
+#	cd `dirname $0`
+#	bundle exec thin --debug -C config/development/thin.yml -R config.ru stop
+#}
 
 
-devel_start
-devel_stop
+#devel_start
+#devel_stop
 
 # For real production :
 #   thin -C thin-prod.yml -R config.ru start
 
 # For development with other constraints
 #   shotgun config.ru -o 0.0.0.0 -p 9393
+
+MYSQL_CONF=$(pwd)/config/development/mysql.conf
+MYSQL_DATA=$(pwd)/tmp/mysql/
+MYSQL_SOCKET=$(pwd)/tmp/mysql.socket
+
+
+#/usr/sbin/mysqld --defaults-file=${MYSQL_CONF} --datadir=${MYSQL_DATA} --socket=${MYSQL_SOCKET}
+
+bundle exec unicorn -p 9393
+
