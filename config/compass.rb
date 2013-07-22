0e13b290a1737ce0f5078d004969fa7369b86f1f
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2013-07-22 11:15:01 +0200

    Add compass configuration.

---
+++ config/compass.rb
@@ -0,0 +1,38 @@
+# Require any additional compass plugins here.
+
+if defined?(Sinatra)
+  # This is the configuration to use when running within sinatra
+  project_path = Sinatra::Application.root
+  environment = :development
+else
+  # this is the configuration to use when running within the compass command line tool.
+  css_dir = File.join 'public', 'stylesheets'
+  relative_assets = true
+  environment = :production
+end
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+http_images_path = "/images"
+http_stylesheet_path = "/stylesheets"
+
+# css_dir = "stylesheets"
+sass_dir = File.join "views", "stylesheets"
+images_dir = File.join "public", "images"
+javascripts_dir = File.join "public", "javascripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
