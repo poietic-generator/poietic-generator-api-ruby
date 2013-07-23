56b566dd6897a3a4bd4bbac1667aebb3de53a921
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2013-07-23 18:12:41 +0200

    Added a configure part for CLI.

--- bin/poietic-cli.rb
+++ bin/poietic-cli.rb
@@ -4,11 +4,27 @@ require 'bundler/setup'
 
 require 'thor'
 
+$:.insert 0, '.'
+require 'poieticgen/config_manager'
+
 module PoieticGen
-
-
-	class CLI < Thor
+	module CLI
 		class Session < Thor
+
+			def configure
+				config = PoieticGen::ConfigManager.new PoieticGen::ConfigManager::DEFAULT_CONFIG_PATH
+				DataMapper::Logger.new(STDERR, :info)
+				#DataMapper::Logger.new(STDERR, :debug)
+				hash = config.database.get_hash
+				#pp "db hash :", hash
+				DataMapper.setup(:default, hash)
+
+				# raise exception on save failure (globally across all models)
+				DataMapper::Model.raise_on_save_failure = true
+
+				DataMapper.auto_upgrade!
+			end
+
 			desc "session list", "List all session"
 			def list
 				puts "FIXME: List all sessions" 
@@ -25,11 +41,13 @@ module PoieticGen
 		end
 
 
-		desc "session SUBCOMMAND ...ARGS", "manage sessions"
-		subcommand "session", Session
+		class CLI < Thor
+			desc "session SUBCOMMAND ...ARGS", "manage sessions"
+			subcommand "session", Session
+		end
 	end
 end
 
 # load libraries
 # choose session
-PoieticGen::CLI.start(ARGV)
+PoieticGen::CLI::Main.start(ARGV)
