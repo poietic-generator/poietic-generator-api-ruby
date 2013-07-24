e73bbc32048f7a16f158ea0d27d3786849c5a11c
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2013-07-24 14:37:49 +0200

    poietic-cli.rb: Implement 'list' and 'delete'.

--- bin/poietic-cli.rb
+++ bin/poietic-cli.rb
@@ -2,20 +2,77 @@
 
 require 'bundler/setup'
 
+require 'dm-core'
+require 'dm-validations'
+require 'dm-migrations'
+require 'dm-transactions'
+require 'dm-types'
 require 'thor'
+require 'pp'
 
 $:.insert 0, '.'
 require 'poieticgen/config_manager'
+require 'poieticgen/session'
 
 module PoieticGen
 	module CLI
 		class Session < Thor
 
+
+			desc "list", "List all session"
+			def list
+				configure
+				sessions = PoieticGen::Session.all	
+				sessions.each do |s|
+					puts "ID % 3d - TOKEN %s - [START %s .. STOP none]" % [ 
+						s.id, s.token, 
+						Time.at(s.timestamp).utc.iso8601
+					]
+				end
+
+			end
+
+			desc "start LABEL", "Start a new session"
+			def create
+				raise NotImplementedError
+			end
+
+			desc "rename OLDLABEL NEWLABEL", "Rename a session"
+			def rename
+				raise NotImplementedError
+			end
+
+			desc "finish", "Finish a session"
+			def finish
+				puts "FIXME: Create A new session" 
+				raise NotImplementedError
+			end
+
+			desc "delete ID", "Delete session ID"
+			def delete id
+				configure
+				session = PoieticGen::Session.first(:id => id.to_i)
+				if session.nil? then
+					puts "ERROR: Session %s does not exist." % id.to_i
+					exit 1
+				end
+
+				session.destroy
+				pp session
+			end
+
+			desc "shapshot OFFSET", "Dump snapshot at OFFSET"
+			def snapshot cmd
+				raise NotImplementedError
+			end
+
+
+			private
 			def configure
-				config = PoieticGen::ConfigManager.new PoieticGen::ConfigManager::DEFAULT_CONFIG_PATH
+				@config = PoieticGen::ConfigManager.new PoieticGen::ConfigManager::DEFAULT_CONFIG_PATH
 				DataMapper::Logger.new(STDERR, :info)
 				#DataMapper::Logger.new(STDERR, :debug)
-				hash = config.database.get_hash
+				hash = @config.database.get_hash
 				#pp "db hash :", hash
 				DataMapper.setup(:default, hash)
 
@@ -24,24 +81,10 @@ module PoieticGen
 
 				DataMapper.auto_upgrade!
 			end
-
-			desc "session list", "List all session"
-			def list
-				puts "FIXME: List all sessions" 
-			end
-
-			desc "session create", "Create a new session"
-			def create
-				puts "FIXME: Create A new session" 
-			end
-
-			desc "session delete ID", "Delete session ID"
-			def delete cmd
-			end
 		end
 
 
-		class CLI < Thor
+		class Main < Thor
 			desc "session SUBCOMMAND ...ARGS", "manage sessions"
 			subcommand "session", Session
 		end
