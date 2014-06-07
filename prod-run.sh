208d688693db356c926c101f7e83c1eeefcd32dd
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2014-06-07 16:20:03 +0200

    Remove obsolete bash scripts.

--- prod-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-PIDFILE="tmp/pids/thin.pid"
-STARTCMD="nohup bundle exec thin -C config/testing/thin.yml -R config.ru start"
-
-if [ -e $PIDFILE ]; then ps uw -p `cat $PIDFILE` | grep -q 'thin' || (rm $PIDFILE; $STARTCMD ); else $STARTCMD ; fi
-
