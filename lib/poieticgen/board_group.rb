4e8b2a4e8119e47279fbbab8004710727bac98fb
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2014-06-28 23:51:20 +0200

    Add board group class.

---
+++ lib/poieticgen/board_group.rb
@@ -0,0 +1,58 @@
+##############################################################################
+#                                                                            #
+#  Poietic Generator Reloaded is a multiplayer and collaborative art         #
+#  experience.                                                               #
+#                                                                            #
+#  Copyright (C) 2011-2013 - Gnuside                                         #
+#                                                                            #
+#  This program is free software: you can redistribute it and/or modify it   #
+#  under the terms of the GNU Affero General Public License as published by  #
+#  the Free Software Foundation, either version 3 of the License, or (at     #
+#  your option) any later version.                                           #
+#                                                                            #
+#  This program is distributed in the hope that it will be useful, but       #
+#  WITHOUT ANY WARRANTY; without even the implied warranty of                #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero  #
+#  General Public License for more details.                                  #
+#                                                                            #
+#  You should have received a copy of the GNU Affero General Public License  #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                            #
+##############################################################################
+
+
+module PoieticGen
+	class BoardGroup
+		include DataMapper::Resource
+
+		property :id,	Serial
+		property :name,          String,  :unique => true, :required=> false
+		property :token, String,  :required => true, :unique => true
+		property :closed,        Boolean, :default => false
+		property :timestamp_start,     Integer, :required => true
+		property :timestamp_stop, Integer, :default => 0
+		property :allocator_type, String, :required => true
+
+		has n, :boards
+
+
+		def self.create config, name
+			res = super({
+				# FIXME: when the token already exists, SaveFailureError is raised
+				:token => (0...16).map{ ('a'..'z').to_a[rand(26)] }.join,
+				:timestamp_start => Time.now.to_i,
+				:allocator_type => config.allocator,
+				:name => name
+			})
+
+			@debug = true
+			rdebug "using allocator %s" % config.allocator
+			return res
+
+		rescue DataMapper::SaveFailureError => e
+			rdebug "Saving failure : %s" % e.resource.errors.inspect
+			raise e
+		end
+	end
+end
+
