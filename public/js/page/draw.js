5e08888212ab0d5e98a83988d64e1bb30ae75497
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2014-08-21 08:41:11 +0200

    Reorganize code in draw.

--- public/js/page/draw.js
+++ public/js/page/draw.js
@@ -35,12 +35,22 @@
 		editor = null,
 		chat = null,
 		bot = null,
+		username = null,
 		KEY_LEFT = 37,
 		KEY_UP = 38,
 		KEY_RIGHT = 39,
 		KEY_DOWN = 40,
 		KEY_A = 65,
-		KEY_B = 66;
+		KEY_B = 66,
+		bot_cur_key = 0, 
+		bot_keys;
+
+	bot_keys = [ KEY_UP, KEY_UP,
+		KEY_DOWN, KEY_DOWN,
+		KEY_LEFT, KEY_RIGHT,
+		KEY_LEFT, KEY_RIGHT,
+		KEY_B,
+	KEY_A ];
 
 	if (PoieticGen.Zone === undefined) {
 		console.error("PoieticGen.Zone is not defined !");
@@ -55,22 +65,33 @@
 		console.error("PoieticGen.Viewer is not defined !");
 	}
 
+	function bot_keydown_handle(event) {
+		if (bot_cur_key < bot_keys.length && event.keyCode === bot_keys[bot_cur_key]) {
+			bot_cur_key += 1;
+
+			if (bot_cur_key >= bot_keys.length) {
+				if (null === bot) {
+					bot = new PoieticGen.Bot(editor);
+				}
+				if (bot.started()) {
+					bot.stop();
+				} else {
+					bot.draw_lines();
+				}
+
+				bot_cur_key = 0;
+			}
+		} else if (bot_cur_key !== 0) {
+			bot_cur_key = 0;
+			bot_keydown_handle(event);
+		} else {
+			bot_cur_key = 0;
+		}
+	}
+
 	// instead of windows.onload
 	$(document).ready(function () {
-		var bot_cur_key = 0, bot_keys;
 
-		bot_keys = [ KEY_UP, KEY_UP,
-			KEY_DOWN, KEY_DOWN,
-			KEY_LEFT, KEY_RIGHT,
-			KEY_LEFT, KEY_RIGHT,
-			KEY_B,
-			KEY_A ];
-
-		$("#usernameDialog form").bind("submit", function (event) {
-			// change username & close popup
-			$('#usernameDialog').popup('close');
-			return false;
-		});
 
 		$(".logout").bind("click", function (event) {
 			event.preventDefault();
@@ -85,13 +106,28 @@
 		session = new PoieticGen.DrawSession(
 			function (session) {
 				//console.log("page_draw/ready: session callback ok");
-				$(".username").text(session.user_name);
+
 
 				board = new PoieticGen.Board(session);
 				editor = new PoieticGen.Editor(session, board, 'session-editor');
 				//var color_picker = new ColorPicker( editor );
 				chat = new PoieticGen.Chat(session);
 				viewer = new PoieticGen.Viewer(session, board, 'session-viewer', editor);
+				username = new PoieticGen.Username({
+					session: session,
+					onupdate: function update_ui(name) {
+						// update various location with name
+						$(".username").text(name);
+						$("#usernameDialog #un").val(name);
+					}
+				});
+
+				$("#usernameDialog form").bind("submit", function (event) {
+					// change username & close popup
+					username.change($('#un').val());
+					$('#usernameDialog').popup('close');
+					return false;
+				});
 
 				//console.log("page_draw/ready: prepicker");
 				$("#brush").bind("vclick", function (event) {
@@ -114,29 +150,7 @@
 		);
 
 		// Bot starts with the KONAMI sequence of keys
-		$(document).bind("keydown", function bot_keydown_handle(event) {
-			if (bot_cur_key < bot_keys.length && event.keyCode === bot_keys[bot_cur_key]) {
-				bot_cur_key += 1;
-
-				if (bot_cur_key >= bot_keys.length) {
-					if (null === bot) {
-						bot = new PoieticGen.Bot(editor);
-					}
-					if (bot.started()) {
-						bot.stop();
-					} else {
-						bot.draw_lines();
-					}
-
-					bot_cur_key = 0;
-				}
-			} else if (bot_cur_key !== 0) {
-				bot_cur_key = 0;
-				bot_keydown_handle(event);
-			} else {
-				bot_cur_key = 0;
-			}
-		});
+		$(document).bind("keydown", bot_keydown_handle);
 	});
 
 }(PoieticGen, jQuery));
