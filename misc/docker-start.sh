8bf0f9c35ae958c149022115dfb35111325af31f
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2015-02-03 23:42:30 +0100

    docker-start: make sure we clean database beforehand.

--- misc/docker-start.sh
+++ misc/docker-start.sh
@@ -4,7 +4,7 @@
 
 SAMPLE=/home/user/poieticgen/config/config.ini.example
 CONFIG=/home/user/poieticgen/config/config.ini
-MYSQL="mysql -h $DB_PORT_3306_TCP_ADDR -u root"
+MYSQL="mysql -h $DB_PORT_3306_TCP_ADDR -u root --raw --batch --silent"
 
 
 echo "Waiting for remote database on $DB_PORT_3306_TCP_ADDR"
@@ -14,10 +14,19 @@ while true ; do
 	if [ $? -eq 0 ]; then break; fi
 done
 
-set -x
+set -xe
 
 echo "Configuring database and permissions"
+mysql_user_exist=$(echo "SELECT count(user) FROM mysql.user WHERE user = 'poieticgen';" | $MYSQL)
+if [ "$mysql_user_exist" -gt 0 ]; then
+	echo "DROP USER 'poieticgen'@'%';" | $MYSQL
+fi
 echo "CREATE USER 'poieticgen'@'%' IDENTIFIED BY 'poieticgen';" | $MYSQL
+
+mysql_db_exist=$(echo "SELECT count(SCHEMA_NAME) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'poieticgen';" | $MYSQL)
+if [ "$mysql_db_exist" -gt 0 ]; then
+	echo "DROP DATABASE poieticgen;" | $MYSQL
+fi
 echo "CREATE DATABASE poieticgen;" | $MYSQL
 echo "GRANT ALL ON poieticgen.* TO 'poieticgen'@'%';" | $MYSQL
 echo "FLUSH PRIVILEGES;" | $MYSQL
@@ -37,16 +46,16 @@ sed -e "s/^host =.*/host = ${DB_PORT_3306_TCP_ADDR}/" \
 	< $SAMPLE \
 	> $CONFIG
 
-su - user -c "cd /home/user/poieticgen ;
-	bundle install --path /home/user/.bundle/ ;
+su - user -c "cd /home/user/poieticgen ; \
+	bundle install --path /home/user/.bundle/"
+
+su - user -c "cd /home/user/poieticgen ; \
 	bundle exec bin/poietic-cli create -n 'Default Session'"
 
 su - user -c "cd /home/user/poieticgen ;
-	bundle install --path /home/user/.bundle/ ;
 	bundle exec bin/poietic-cli create -n 'Test session A'"
 
 su - user -c "cd /home/user/poieticgen ;
-	bundle install --path /home/user/.bundle/ ;
 	bundle exec bin/poietic-cli create -n 'Test session B'"
 
 echo "Starting application"
