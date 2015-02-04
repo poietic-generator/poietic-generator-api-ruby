b2a4cd5e927d04967498c6a84fa094e26e37ecf7
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2015-02-04 10:15:39 +0100

    spec: Add a directory for future tests/specs.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+#require 'mark'
+require 'minitest/unit'
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'minitest/pride'
+
+$LOAD_PATH.unshift('../lib')
+
+#if __FILE__ == $0
+#  $LOAD_PATH.unshift('lib', 'spec')
+#           Dir.glob('./spec/**/*_spec.rb') { |f| require f }
+#end
+
