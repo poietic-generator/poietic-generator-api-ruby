71234f8d114abd41ef36b52fa3b321640612152e
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2015-12-08 08:34:02 +0100

    Simplify rackup launcher.

--- config.ru
+++ config.ru
@@ -1,21 +1,5 @@
 
-require 'active_support'
-
-require 'dm-core'
-require 'dm-validations'
-require 'dm-migrations'
-require 'dm-transactions'
-require 'dm-types'
-require 'dm-constraints'
-
-require 'compass'
-require 'haml'
-require 'sass'
-require 'pp'
-require 'duration'
-
-#require 'pry'
-
+# start profiling before requiring sinatra
 $:.insert(0,"lib")
 
 require 'poieticgen'
--- lib/poieticgen.rb
+++ lib/poieticgen.rb
@@ -1,6 +1,29 @@
 
+# require 'pry'
+# require 'ruby-prof'
+# RubyProf.start
+# RubyProf.pause
+# 
+# at_exit do
+#   result = RubyProf.stop
+#   printer = RubyProf::MultiPrinter.new(result)
+#   printer.print(:path => ".", :profile => "profile")
+# end
+# 
 require 'tilt/haml'
 require 'tilt/sass'
+
+require 'dm-core'
+require 'dm-validations'
+require 'dm-migrations'
+require 'dm-transactions'
+require 'dm-types'
+require 'dm-constraints'
+
+require 'compass'
+require 'haml'
+require 'sass'
+require 'pp'
 
 require "poieticgen/version"
 require 'poieticgen/board'
@@ -9,5 +32,5 @@ require 'poieticgen/config_manager'
 require 'poieticgen/manager'
 require "poieticgen/zone"
 require "poieticgen/zone_snapshot"
-require "poieticgen/cli"
+require "poieticgen/user"
 
