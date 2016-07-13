2f06408cef6f764da2555a97acf1094c76c0a8e8
Glenn Y. Rolland <glenn.rolland@netcat.io>
2016-07-13 23:42:53 +0200

    Remove useless transaction error management.

--- lib/poieticgen/manager.rb
+++ lib/poieticgen/manager.rb
@@ -192,71 +192,59 @@ module PoieticGen
 
 			# prepare empty result message
 			result = {
-					events: [],
-					strokes: [],
-					messages: [],
+				events: [],
+				strokes: [],
+				messages: [],
 			}
 			now = Time.now.to_i
 
-			User.transaction do |t| begin
-				user = User.first(token: req.user_token)
-				if user.nil? or user.expired? then
-					raise InvalidSession, "Session has expired!"
-				end
+			User.transaction do |t|
+			  user = User.first(token: req.user_token)
+			  if user.nil? or user.expired? then
+				  raise InvalidSession, "Session has expired!"
+			  end
 
-				board = user.board
-				if board.nil? or
-				   board.closed or
-				   board.board_group.token != req.session_token then
-					raise InvalidSession, "No opened session found for board %s" % req.session_token
-				end
+			  board = user.board
+			  if board.nil? or
+				  board.closed or
+				  board.board_group.token != req.session_token then
+				  raise InvalidSession, "No opened session found for board %s" % req.session_token
+			  end
 
-				ref_stamp = user.last_update_time - req.update_interval
+			  ref_stamp = user.last_update_time - req.update_interval
 
-				user.alive_expires_at = now + @config.user.liveness_timeout
-				if not req.strokes.empty? then
-					user.idle_expires_at = now + @config.user.idle_timeout
-				end
-				user.last_update_time = now
+			  user.alive_expires_at = now + @config.user.liveness_timeout
+			  if not req.strokes.empty? then
+				  user.idle_expires_at = now + @config.user.idle_timeout
+			  end
+			  user.last_update_time = now
 
-				user.save
+			  user.save
 
-				begin
-					board.update_data user, req.strokes
-				rescue PoieticGen::TakeSnapshotError => e
-					# Ignore error
-					STDERR.puts "Take snapshot error ignored!"
-				end
-				@chat.update_data user, req.messages
-				
-				timelines = board.timelines.all(
-					:id.gt => req.timeline_after
-				)
+				board.update_data user, req.strokes
+			  @chat.update_data user, req.messages
 
-				strokes = timelines.strokes.all(
-					:zone.not => user.zone
-				)
+			  timelines = board.timelines.all(
+				  :id.gt => req.timeline_after
+			  )
 
-				strokes_collection = strokes.map{ |d| d.to_hash ref_stamp }
-				events_collection = timelines.events.map{ |event| event.to_hash ref_stamp }
-				messages = timelines.messages.all(
-					user_dst: user.id
-				)
-				messages_collection = messages.map{ |message| message.to_hash }
+			  strokes = timelines.strokes.all(
+				  :zone.not => user.zone
+			  )
 
-				result = {
-					events: events_collection,
-					strokes: strokes_collection,
-					messages: messages_collection,
-				}
+			  strokes_collection = strokes.map{ |d| d.to_hash ref_stamp }
+			  events_collection = timelines.events.map{ |event| event.to_hash ref_stamp }
+			  messages = timelines.messages.all(
+				  user_dst: user.id
+			  )
+			  messages_collection = messages.map{ |message| message.to_hash }
 
-				rescue DataObjects::TransactionError => e
-					Transaction.handle_deadlock_exception e, t, "Manager.update_data"
+			  result = {
+				  events: events_collection,
+				  strokes: strokes_collection,
+				  messages: messages_collection,
+			  }
 
-				rescue Exception => e
-					t.rollback
-					raise e
-				end
 			end
 
 			return result
