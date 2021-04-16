bd25e3fe5e33bc70228cd17324a6136c9afd8e8d
Glenn Y. Rolland <glenux@glenux.net>
2021-04-16 02:21:22 +0200

    fix: adjustments for modern systems

--- misc/docker-start-postgres.sh
+++ misc/docker-start-postgres.sh
@@ -2,20 +2,20 @@
 
 # copy config + use environement variables
 
-sleep 1
+echo "ENVIRONMENT:"
+env
 
-SAMPLE=/home/user/poieticgen/config/config.ini.example
-CONFIG=/home/user/poieticgen/config/config.ini
-
-PSQL="psql postgresql://$DB_ENV_POSTGRES_USER:$DB_ENV_POSTGRES_PASSWORD@$DB_PORT_5432_TCP_ADDR:$DB_PORT_5432_TCP_PORT/$DB_ENV_POSTGRES_DB"
+SAMPLE=/app/config/config.ini.example
+CONFIG=/app/config/config.ini
 
 # set -x
 
-while true ; do
-	echo "Waiting for remote database on $DB_PORT_5432_TCP_ADDR"
+echo "Waiting for remote database on $POSTGRES_HOST"
+COUNT=0
+while psql  psql -U "$POSTGRES_USER" -h "$POSTGRES_HOST" "$POSTGRES_DB" $DATABASE_URL  -c '\q' ; do
 	sleep 1s
-	$PSQL -c '\q'
-	if [ "$?" -eq 0 ]; then break; fi
+	COUNT=$((COUNT + 1))
+	test $COUNT -gt 300 && echo "Timed out" && exit 1
 done
 
 set -xe
@@ -25,35 +25,28 @@ set -xe
 #CPUPROFILE_FREQUENCY=1000
 #RUBYOPT="-r`gem which perftools | tail -1`"
 echo "Configuring application"
-echo "PORT=8000" > /home/user/poieticgen/.env 
-chown -R user:user /home/user/poieticgen/.env
+echo "PORT=8000" > /app/.env 
 
-sed -e "s/^host =.*/host = ${DB_PORT_5432_TCP_ADDR}/" \
+sed -e "s/^host =.*/host = ${POSTGRES_HOST}/" \
+	-e "s/^port = .*/port = 5432/" \
 	-e "s/^adapter = .*/adapter = postgres/" \
-	-e "s/^database = .*/database = $DB_ENV_POSTGRES_DB/" \
-	-e "s/^username = .*/username = $DB_ENV_POSTGRES_USER/" \
-	-e "s/^password = .*/password = $DB_ENV_POSTGRES_PASSWORD/" \
+	-e "s/^database = .*/database = $POSTGRES_DB/" \
+	-e "s/^username = .*/username = $POSTGRES_USER/" \
+	-e "s/^password = .*/password = $POSTGRES_PASSWORD/" \
 	-e "s/^admin_username = .*/admin_username = admin/" \
 	-e "s/^admin_password = .*/admin_password = admin/" \
 	< $SAMPLE \
 	> $CONFIG
 
-su - user -c "cd /home/user/poieticgen ; \
-	bundle install --path /home/user/.bundle/"
+bundle install
 
 # Create default sessions if database was new
-if ! su - user -c "cd /home/user/poieticgen ; bundle exec bin/poietic-cli list |grep 'Test session B'" ; then
-
-	su - user -c "cd /home/user/poieticgen ; \
-		bundle exec bin/poietic-cli create -n 'Default Session'"
-
-	su - user -c "cd /home/user/poieticgen ;
-	bundle exec bin/poietic-cli create -n 'Test session A'"
-
-	su - user -c "cd /home/user/poieticgen ;
-	bundle exec bin/poietic-cli create -n 'Test session B'"
+if ! bundle exec bin/poietic-cli list |grep 'Test session B' ; then
+  bundle exec bin/poietic-cli create -n 'Default Session'
+  bundle exec bin/poietic-cli create -n 'Test session A'
+  bundle exec bin/poietic-cli create -n 'Test session B'
 fi
 
 echo "Starting application"
-exec su - user -c 'cd /home/user/poieticgen ; bundle exec foreman start'
+exec bundle exec foreman start
 
